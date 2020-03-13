0c9d9b7aaaef8fbb817d3925bb3297316695a994
João Carlos Brasileiro <j.brasileiro@sap.com>
2020-03-13 00:09:25 -0300

    add notEmpty/isEmpty

--- src/main/java/com/github/osphuhula/gitrepositorygenerator/util/CollectionUtils.java
+++ src/main/java/com/github/osphuhula/gitrepositorygenerator/util/CollectionUtils.java
@@ -2,15 +2,26 @@ package com.github.osphuhula.gitrepositorygenerator.util;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class CollectionUtils {
 
-    private CollectionUtils() {
-        super();
-        throw new IllegalStateException(getClass().getSimpleName());
-    }
+	private CollectionUtils() {
+		super();
+		throw new IllegalStateException(getClass().getSimpleName());
+	}
 
-    public static <T> Collection<T> newLinkedList() {
-        return new LinkedList<>();
-    }
+	public static <T> Collection<T> newLinkedList() {
+		return new LinkedList<>();
+	}
+
+	public static boolean notEmpty(
+		Collection<?> collection) {
+		return !isEmpty(collection);
+	}
+
+	private static boolean isEmpty(
+		Collection<?> collection) {
+		return Objects.isNull(collection) || collection.isEmpty();
+	}
 }
